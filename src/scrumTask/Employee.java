3319518fbef34a699cc01995e7f1c4f4de13c6e3
Meastn <av.melihcalikoglu@protonmail.com>
2022-08-23 22:59:51 +0300

    Initial commit

---
+++ src/scrumTask/Employee.java
@@ -0,0 +1,2 @@
+package scrumTask;public class Employee {
+}
---
+++ src/scrumTask/FirstPage.java
@@ -0,0 +1,15 @@
+import scrumTask.Person;
+
+import javax.imageio.plugins.tiff.ExifInteroperabilityTagSet;
+
+public class FirstPage {
+
+    
+    public static void main(String[] args) {
+
+        Person kisi = new Person("George", -10,'M');
+        Person kişi2 = new Person();
+
+        System.out.println(kisi);
+    }
+}
